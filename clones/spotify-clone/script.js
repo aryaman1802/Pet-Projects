45ffbc1681ba01c5121e8d6ef65be84371475875
Aryaman <aryaman1802@gmail.com>
2024-09-26 15:13:49 -0400

    reuploading clones project

---
+++ clones/spotify-clone/script.js
@@ -0,0 +1,11 @@
+if (window.navigator.onLine){
+    console.log('You are online');
+}
+else {
+    console.log('You are offline');
+}
+
+
+
+// To run the above program, open the terminal and navigate to the directory of this file and run the following command:
+// node script.js
